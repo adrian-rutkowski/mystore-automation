e2c1b49565464c0c85d4465eccdf86db6c8b9c0b
adrian-rutkowski <adrianthetranslator@gmail.com>
2020-04-30 17:31:47 +0200

    Initial commit

--- base/basepage.py
+++ base/basepage.py
@@ -4,11 +4,5 @@ from base.selenium_driver import SeleniumDriver
 class BasePage(SeleniumDriver):
 
     def __init__(self, driver):
-        """
-        Inits BasePage class
-
-        Returns:
-            None
-        """
         super(BasePage, self).__init__(driver)
         self.driver = driver
