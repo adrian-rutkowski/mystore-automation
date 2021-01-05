9928f5e1f1e05f0c0f05b7ba95ba959b7e549b43
adrian-rutkowski <adrianthetranslator@gmail.com>
2021-01-05 15:47:08 +0100

    Ability to specify a browser (Chrome/Firefox supported) as a command line argument when running pytest added. If not argument is specified - Firefox runs by default.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 import pytest
 
+def pytest_addoption(parser):
+	parser.addoption(
+		"--browser", action="store", default="firefox")
+
 
 @pytest.yield_fixture()
 def setUp(request):
+	browser = request.config.getoption("--browser")
 	base_url = "http://automationpractice.com/index.php"
-	driver = webdriver.Firefox()
+	if browser == "firefox":
+		driver = webdriver.Firefox()
+	else:
+		driver = webdriver.Chrome()
+
 	driver.maximize_window()
 	driver.get(base_url)
 	request.cls.driver = driver
